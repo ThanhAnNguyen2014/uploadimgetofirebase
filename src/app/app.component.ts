5efb48222e0fa5369ab8de01e220f03462f10984
Thanh An Nguyen <Thanh An Nguyen>
2017-06-05 15:03:39 +0700

    demo upload imge to firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import * as firebase from 'firebase';
+import { FirebaseApp } from 'angularfire2';
+import { Observable } from 'rxjs';
+declare var $: any;
+
+export class Hero {
+  title: string;
+  link: string;
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements OnInit {
+  url: any;
+  image: any;
+  folder = 'fsl-io';
+  files: File;
+  filesToUpload: Array<File>;
+  hero: Hero={
+    title: null,
+    link:null
+  };
+
+  constructor( @Inject(FirebaseApp) firebaseApp: any) {
+    const storageRef = firebase.storage().ref().child('fsl-io/abc.png');
+    storageRef.getDownloadURL().then(url => {
+      this.image = url;
+
+      console.log(url);
+    });
+  }
+  ngOnInit() {
+    //$.getScript('https://www.gstatic.com/firebasejs/4.0.0/firebase.js');
+
+  }
+
+
+
+  fileChangeEvent(fileInput: any) {
+    this.filesToUpload = <Array<File>>fileInput.target.files;
+    this.files = fileInput.target.files[0];
+    console.log(this.files);
+
+    let storageRef = firebase.storage().ref();
+
+    let path = `/${this.folder}/${this.files.name}`;
+    let iRef = storageRef.child(path);
+    iRef.put(this.files).then((snapshot) => {
+      //this.image = selectedFile.name;
+      //this.url = path;
+      this.url = snapshot.downloadURL;
+      console.log(this.url);
+    });
+  }
+
+  onSubmit(f: NgForm) {
+    console.log(f.value);
+
+    this.hero.title = f.value.title;
+    this.hero.link=this.url;
+
+    console.log(this.hero);
+    // Create root ref
+
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+/**Import component and module of firebase */
+import { AngularFireModule} from 'angularfire2';
+
 import { AppComponent } from './app.component';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBdgpxShqNc5-cwvu9MPYk0b4ejHpSNnKY",
+  authDomain: "fsl-io.firebaseapp.com",
+  databaseURL: "https://fsl-io.firebaseio.com",
+  projectId: "fsl-io",
+  storageBucket: "fsl-io.appspot.com",
+  messagingSenderId: "108922144834"
+};
 
 @NgModule({
   declarations: [
@@ -11,8 +24,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     FormsModule,
-    HttpModule
+    HttpModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
